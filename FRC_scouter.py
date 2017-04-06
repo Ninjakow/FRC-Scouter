6f1b4f88f827a5228336358c2cba1b85ff53b8db
Joel Bryla <joel@wiredlizard.com>
2017-04-06 15:14:08 -0600

    added quiting and not found exiting

--- FRC_scouter.py
+++ FRC_scouter.py
@@ -4,14 +4,23 @@ import csv
 
 team_names = {}
 
+#with open('eggs.csv', newline='') as csvfile:
 
 print ("Enter Event Key:")
-event_key = input()
+event_key = input().lower()
 event_key = "2017"+event_key
+if event_key == 'q':
+	quit()
 
 
 rankings = requests.get("https://www.thebluealliance.com/api/v2/event/"+event_key+"/rankings", headers={"X-TBA-App-Id":"frc-4774:FRC_auto_scouter:1.0"})
 rankings = rankings.json()
+if len(rankings) == 0:
+		print("No information")
+		quit()
+elif rankings["404"]:
+	print(rankings["404"])
+	quit()
 
 teams = requests.get("https://www.thebluealliance.com/api/v2/event/"+event_key+"/teams", headers={"X-TBA-App-Id":"frc-4774:FRC_auto_scouter:1.0"})
 teams = teams.json()
@@ -28,6 +37,7 @@ while True:
 	team_key = input()
 	if team_key == "q":
 		break
+
 	for item in rankings:
 		if item[0] == 'Rank':
 			continue
@@ -50,3 +60,4 @@ while True:
 		stats.percentileofscore(gear, gear[item[0]-1]), stats.percentileofscore(balls, balls[item[0]-1])))
 
 
+quit()
