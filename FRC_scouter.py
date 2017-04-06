12407268bd5bd2e82a978257bf0397fbc9f06777
Joel Bryla <joel@wiredlizard.com>
2017-04-06 17:23:11 -0600

    Only calculates percentiles for teams which have that ability + added scouting sheet

--- FRC_scouter.py
+++ FRC_scouter.py
@@ -1,10 +1,17 @@
 import requests
-from scipy import stats
 import csv
+from scipy import stats
+
+# pylint: disable=W0312,C0325,C0103,C0301,C0330
 
 team_names = {}
+abilities = {}
 
-#with open('eggs.csv', newline='') as csvfile:
+with open('Calgary_Scouting.csv', newline='') as csvfile:
+	reader = csv.DictReader(csvfile)
+	for row in reader:
+		abilities[row["Team number"]] = row["What can it do?"].split(";")
+		abilities[row["Team number"]].append(row["Does it have auto?"])
 
 print ("Enter Event Key:")
 event_key = input().lower()
@@ -12,13 +19,16 @@ event_key = "2017"+event_key
 if event_key == 'q':
 	quit()
 
-
 rankings = requests.get("https://www.thebluealliance.com/api/v2/event/"+event_key+"/rankings", headers={"X-TBA-App-Id":"frc-4774:FRC_auto_scouter:1.0"})
 rankings = rankings.json()
 if len(rankings) == 0:
-		print("No information")
-		quit()
-elif rankings["404"]:
+	print("No information")
+	quit()
+try:
+	rankings["404"]
+except:
+	pass
+else:
 	print(rankings["404"])
 	quit()
 
@@ -39,14 +49,34 @@ while True:
 		break
 
 	for item in rankings:
+		can_climb = False
+		can_gear = False
+		can_balls = False
+		can_auto = False
+
 		if item[0] == 'Rank':
 			continue
-
-		auto.append(item[4])
-		climb.append(item[6])
-		gear.append(item[5])
-		balls.append(item[7])
-
+		try:
+			for skill in abilities[str(item[1])]:
+				if skill == "Gear":
+					can_gear = True
+				elif skill == "Climber":
+					can_climb = True
+				elif skill == "Shooter" or skill == "Loader":
+					can_balls = True
+				elif skill == 'Yes':
+					can_auto = True
+		except:
+			pass
+		
+		if can_auto:
+			auto.append(item[4])
+		if can_climb:
+			climb.append(item[6])
+		if can_gear:
+			gear.append(item[5])
+		if can_balls:
+			balls.append(item[7])
 
 	for item in rankings:
 		if item[0] == 'Rank':
@@ -56,8 +86,7 @@ while True:
 				continue
 		print ("")
 		print ("%s. %s - %s \nAuto: %s \nClimb: %s \nGear: %s \nShooting: %s" %(item[0], item[1], team_names[item[1]],
-		stats.percentileofscore(auto, auto[item[0]-1]), stats.percentileofscore(climb, climb[item[0]-1]),
-		stats.percentileofscore(gear, gear[item[0]-1]), stats.percentileofscore(balls, balls[item[0]-1])))
-
+		stats.percentileofscore(auto, item[4]), stats.percentileofscore(climb, item[6]),
+		stats.percentileofscore(gear, [5]), stats.percentileofscore(balls, item[7])))
 
 quit()
