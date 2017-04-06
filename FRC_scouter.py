8b3abe6981fb6579f812be58cf54b650baa5a877
Joel Bryla <joel@wiredlizard.com>
2017-04-06 14:56:23 -0600

    Converted to python 3

--- FRC_scouter.py
+++ FRC_scouter.py
@@ -1,20 +1,22 @@
 import requests
 from scipy import stats
+import csv
 
 team_names = {}
 
-print "Enter Event Key:"
-event_key = raw_input()
+
+print ("Enter Event Key:")
+event_key = input()
 event_key = "2017"+event_key
 
 
-rankings = requests.get("https://www.thebluealliance.com/api/v2/event/"+event_key+"/rankings", headers={"X-TBA-App-Id":"frc-4774:Scouting_percentiles:1.0"})
+rankings = requests.get("https://www.thebluealliance.com/api/v2/event/"+event_key+"/rankings", headers={"X-TBA-App-Id":"frc-4774:FRC_auto_scouter:1.0"})
 rankings = rankings.json()
 
-teams = requests.get("https://www.thebluealliance.com/api/v2/event/"+event_key+"/teams", headers={"X-TBA-App-Id":"frc-4774:Scouting_percentiles:1.0"})
+teams = requests.get("https://www.thebluealliance.com/api/v2/event/"+event_key+"/teams", headers={"X-TBA-App-Id":"frc-4774:FRC_auto_scouter:1.0"})
 teams = teams.json()
 for team in teams:
-    team_names[team["team_number"]] = team["nickname"]
+	team_names[team["team_number"]] = team["nickname"]
 
 # create lists of stats
 auto = []
@@ -22,32 +24,29 @@ climb = []
 gear = []
 balls = []
 while True:
-	print "Enter Team Number or ENTER for all:"
-	team_key = raw_input()
+	print ("Enter Team Number or ENTER for all:")
+	team_key = input()
 	if team_key == "q":
-	    break
+		break
 	for item in rankings:
-	    if item[0] == 'Rank':
-		continue
+		if item[0] == 'Rank':
+			continue
 
-	    auto.append(item[4])
-	    climb.append(item[6])
-	    gear.append(item[5])
-	    balls.append(item[7])
+		auto.append(item[4])
+		climb.append(item[6])
+		gear.append(item[5])
+		balls.append(item[7])
 
-	print ""
-	print len(team_key)
+
 	for item in rankings:
-	    if item[0] == 'Rank':
-		continue
-	    if len(team_key) > 0:
-		if team_key != str(item[1]):
-		    continue
+		if item[0] == 'Rank':
+			continue
+		if len(team_key) > 0:
+			if team_key != str(item[1]):
+				continue
+		print ("")
+		print ("%s. %s - %s \nAuto: %s \nClimb: %s \nGear: %s \nShooting: %s" %(item[0], item[1], team_names[item[1]],
+		stats.percentileofscore(auto, auto[item[0]-1]), stats.percentileofscore(climb, climb[item[0]-1]),
+		stats.percentileofscore(gear, gear[item[0]-1]), stats.percentileofscore(balls, balls[item[0]-1])))
 
-	    print ""
-	    print "%s. %s - %s \nAuto: %s \nClimb: %s \nGear: %s \nShooting: %s" %(item[0], item[1], team_names[item[1]],
-		     stats.percentileofscore(auto, auto[item[0]-1]), stats.percentileofscore(climb, climb[item[0]-1]),
-		     stats.percentileofscore(gear, gear[item[0]-1]), stats.percentileofscore(balls, balls[item[0]-1]))
 
-
-quit()
