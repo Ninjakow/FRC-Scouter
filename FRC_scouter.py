11c9a42090c7da70d72fd334a3218cb02870b41e
Joel Bryla <joel@wiredlizard.com>
2017-04-07 15:27:04 -0600

    added git ignore for csv data

--- FRC_scouter.py
+++ FRC_scouter.py
@@ -10,7 +10,13 @@ team_names = {}
 abilities = {}
 trueskills = {}
 
-with open('Calgary_Scouting.csv', newline='') as csvfile:
+print ("Enter Event Key:")
+event_key = input().lower()
+event_key = "2017"+event_key
+if event_key == 'q':
+	quit()
+
+with open("Scouting_Data/"+event_key+'.csv', newline='') as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
 		abilities[row["Team number"]] = dict([("What can it do?", row["What can it do?"].split(";"))])
@@ -20,12 +26,6 @@ with open('Calgary_Scouting.csv', newline='') as csvfile:
 		abilities[row["Team number"]]["Ball Pickup"] = row["Ball Pickup"].split(";")
 		abilities[row["Team number"]]["Auto Abilities"] = row["Auto Abilities"].split(";")
 		abilities[row["Team number"]]["Movement Type"] = row["Movement Type"]
-
-print ("Enter Event Key:")
-event_key = input().lower()
-event_key = "2017"+event_key
-if event_key == 'q':
-	quit()
 
 rankings = requests.get("https://www.thebluealliance.com/api/v2/event/"+event_key+"/rankings", headers={"X-TBA-App-Id":"frc-4774:FRC_auto_scouter:1.0"})
 rankings = rankings.json()
@@ -112,10 +112,10 @@ while True:
 
 		print ("")
 		print ("%s. %s - %s (%s) \nAuto: %s - %s \nClimb: %s \nGear: %s - %s - %s Placement \nBalls: %s - %s \n%s" %(item[0], item[1], team_names[item[1]], (round(trueskills[item[1]]*100)/100),
-		stats.percentileofscore(auto, item[4]), ", ".join(abilities[str(item[1])]["Auto Abilities"]), 
-		stats.percentileofscore(climb, item[6]), 
-		stats.percentileofscore(gear, item[5]), ", ".join(abilities[str(item[1])]["Gear Pickup"]), abilities[str(item[1])]["Gear Placement"], 
-		stats.percentileofscore(balls, item[7]), ", ".join(abilities[str(item[1])]["Ball Pickup"]),
+		(round(stats.percentileofscore(auto, item[4])*100)/100), ", ".join(abilities[str(item[1])]["Auto Abilities"]), 
+		(round(stats.percentileofscore(climb, item[6])*100)/100), 
+		(round(stats.percentileofscore(gear, item[5])*100)/100), ", ".join(abilities[str(item[1])]["Gear Pickup"]), abilities[str(item[1])]["Gear Placement"], 
+		(round(stats.percentileofscore(balls, item[7])*100)/100), ", ".join(abilities[str(item[1])]["Ball Pickup"]),
 		abilities[str(item[1])]["Movement Type"]))
 
 quit()
